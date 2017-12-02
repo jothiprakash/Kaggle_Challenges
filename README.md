e5d5f815b0b52ecd5b3157596387d2184613dd8f
jothiprakash <jothiprakashanandan@gmail.com>
2017-12-02 19:10:39 +0530

    Walmart and Titanic challenge Updated

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Kaggle_Challenges
 Kaggle based machine learning challenges.
+
+# Completed
+
+Titanic_Survival - solution using **Random Forest algorithm**
+
+Walmart_Trip_Type_Classification - solution using **Classification algorithm**
+
+Walmart_Trip_Type - Solution using **clustering algorithm**
---
+++ Walmart_Trip_Type/scripts/Main.R
@@ -0,0 +1,13 @@
+#MainScript
+#produces the train and test datasets
+#Refer to new_fDefinitions.R for the function definitions
+source('scripts/feature_engineering.R')
+
+# Generates the features on Department and fineline through 
+# Kmeans(Unsupervised Learning from trainData)
+# Clustering Departments and fineline items based on their 
+# class distributions in the train data.
+source('scripts/ClusterFeatures.R')
+
+#Loads the dataset and produces the prediction for test data
+source('scripts/xgboost_single.R')
